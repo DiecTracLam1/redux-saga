2740accdf49a97a27e73770b000e217aed05940a
Diec Trac Lam <diectraclam.0812@gmail.com>
2023-03-13 00:26:55 +0700

    Leeson 11

---
+++ src/api/cityApi.ts
@@ -0,0 +1,10 @@
+import axiosClient from './axoisClient';
+
+const cityApi = {
+  getAll() {
+    const url = '/cities';
+    return axiosClient.get(url);
+  },
+};
+
+export default cityApi;
---
+++ src/api/studentApi.ts
@@ -0,0 +1,5 @@
+const studentApi = {
+
+}
+
+export default studentApi
